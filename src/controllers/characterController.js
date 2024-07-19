bfb52ba5093da7278ce97cc2d8412fea72ac9f87
jonasi deetnes <jonasi@inksane.be>
2024-07-19 14:48:25 +0200

    Update prisma schema with charactercontroller

--- src/controllers/characterController.js
+++ src/controllers/characterController.js
@@ -1,10 +1,10 @@
-import prisma from '../prisma.js';
+import prisma from "../prisma.js";
 
 const getCharacters = async (req, res) => {
   const { userId } = req.query;
   try {
     const allCharacters = await prisma.character.findMany({
-      where: { userId: userId }
+      where: { userId: userId },
       // include: {
       //   race: true,
       //   class: true,
@@ -23,7 +23,7 @@ const getCharacters = async (req, res) => {
 
     return res.status(201).json({ allCharacters });
   } catch (error) {
-    return res.status(500).json({ msg: 'Server error' });
+    return res.status(500).json({ msg: "Server error" });
   }
 };
 
@@ -31,27 +31,75 @@ const addCharacter = async (req, res) => {
   const { characterData } = req.body;
   try {
     console.log(characterData);
-    // const { languages, ...characterInfo } = characterData;
-    // const character = await prisma.character.create({
-    //   data: {
-    //     ...characterInfo,
-    //     languages: {
-    //       create: languages.map(language => ({
-    //         language: { connect: { id: language.id } }
-    //       }))
-    //     }
-    //   },
-    //   include: {
-    //     languages: {
-    //       include: {
-    //         language: true
-    //       }
-    //     }
-    //   }
-    // });
-    // return res.status(201).json({ character });
+    const {
+      languages,
+      skills,
+      items,
+      abilities,
+      spells,
+      senses,
+      race,
+      subrace,
+      class: charClass,
+      subclass,
+      ...characterInfo
+    } = characterData;
+
+    const character = await prisma.character.create({
+      data: {
+        ...characterInfo,
+        race: { connect: { id: race.id } },
+        subrace: subrace ? { connect: { id: subrace.id } } : undefined,
+        class: { connect: { id: charClass.id } },
+        subclass: subclass ? { connect: { id: subclass.id } } : undefined,
+        languages: {
+          connect: languages.map((language) => ({
+            id: language.id,
+          })),
+        },
+        skills: {
+          connect: skills.map((skill) => ({
+            id: skill.id,
+          })),
+        },
+        items: {
+          connect: items.map((item) => ({
+            id: item.id,
+          })),
+        },
+        senses: {
+          connect: senses.map((sense) => ({
+            id: sense.id,
+          })),
+        },
+        spells: {
+          connect: spells.map((spell) => ({
+            id: spell.id,
+          })),
+        },
+        abilities: {
+          connect: abilities.map((ability) => ({
+            id: ability.id,
+          })),
+        },
+      },
+      include: {
+        race: true,
+        subrace: true,
+        class: true,
+        subclass: true,
+        languages: true,
+        skills: true,
+        items: true,
+        senses: true,
+        spells: true,
+        abilities: true,
+      },
+    });
+
+    return res.status(201).json({ character });
   } catch (error) {
-    return res.status(500).json({ msg: 'Server error' });
+    return res.status(500).json({ msg: "Server error" });
   }
 };
 
